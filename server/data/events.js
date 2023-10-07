dd95db79675a294ebe00102620d364da3acc2e50
Selishaa <selishathapa5@gmail.com>
2023-10-07 02:41:04 -0400

    Project 3 completed

---
+++ server/data/events.js
@@ -0,0 +1,83 @@
+const eventsData = [
+  {
+    id: 1,
+    image: "https://www.mensjournal.com/.image/t_share/MjAwNDY5NzI0MDEyODE1NDY4/eras-tour-1.jpg",
+    name: "Eras Tour",
+    date: "Nov 3,2024",
+    location: "LucasOil Stadium,Indianapolis",
+    
+    perfomer: "Taylor Swift",
+    
+    },
+    {
+    id: 2,
+    image: "https://i.ebayimg.com/images/g/qDkAAOSwmRNaxXC4/s-l1600.jpg",
+    name: "A Beautiful Blur",
+    date: "Feb 16,2024",
+    location: "Municipal Auditorium,Nashville",
+    perfomer: "Lany",
+    
+    },
+    {
+    id: 3,
+    image: "https://assets.teenvogue.com/photos/6501d6339bd096cfa276a8ab/2:3/w_720,h_1080,c_limit/GUTS%20world%20tour%20poster.jpg",
+    name: "GUTS",
+    date: "March 23,2024",
+    location: "Detroit,Miami",
+    perfomer: "Olivia Rodrigo",
+    
+    },
+    {
+    id: 4,
+    image: "https://kenh14cdn.com/203336854389633024/2023/6/27/photo-4-16878551982741814260947.png",
+    name: "Born Pink",
+    date: "Aug 26,2023",
+    location: "Dodger Stadium,Los Angeles",
+    perfomer: "Black Pink",
+    
+    },
+    {
+    id: 5,
+    image: "https://www.mercurynews.com/wp-content/uploads/2019/10/ecct0420lana01.jpg?w=1024",
+    name: "Tunnel Under Ocean",
+    date: "Sep 17,2023",
+    location: "Austin,Texas",
+    perfomer: "Lana Del Ray",
+    },
+    
+    {
+    id: 6,
+    image: "https://i.pinimg.com/736x/37/c5/cc/37c5ccc5e151bc95bad7bc2ca9ee2efa.jpg",
+    name: "After Hours",
+    date: " Jul 15,2022",
+    location: "AT&T Stadium,Dallas",
+    perfomer: "The Weekend",
+    
+    
+    },
+    {
+    id: 7,
+    image: "https://i.ebayimg.com/images/g/pHwAAOSwEMRi~VXD/s-l1200.jpg",
+    name: "Nobody is Listening",
+    date: "Jan 5,2024",
+    location: "AT&T Stadium,Dallas",
+    perfomer: "Zayn Malik",
+    
+    
+    },
+    {
+    id: 8,
+    image: "https://d.newsweek.com/en/full/1552122/harry-styles-tour-2019.jpg",
+    name: "Love ON Tour",
+    date: "Oct 5,2023",
+    location: "LucasOil Stadium,Indianapolis",
+    perfomer: "Harry Styles",
+    
+    
+    },
+    
+  ];
+  
+  // You can add more event objects with real data as needed
+  
+export default eventsData;
